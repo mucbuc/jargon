16951edbf59e64677021c4eda0040133df64f915
mbusenitz <mbusenitz@gmail.com>
2015-01-07 19:39:58 -0800

    formatter test

--- test/formatter.js
+++ test/formatter.js
@@ -1,17 +1,37 @@
 #!/usr/bin/env node
 
 var assert = require( 'assert' )
-  , events = require( 'events' )
+  , Expector = require( 'expector' ).SeqExpector
   , Formatter = require( '../src/formatter.js' );
 
 assert( typeof Formatter === 'function' );
 
-// suite( 'formatter', function() {
-// 	test( 'basic', function() {
-		var formatter = new Formatter();
+suite( 'formatter', function() {
+	test( 'headingSpaces', function() {
+		var formatter = new Formatter()
+		  , controller = new Expector();
+
+		controller
+			.expect( 'format', '\t \t \n ' )
+			.expect( 'ere', 'hello' )
+			.expect( 'end' );
 
 		formatter.forward( 'ere', '\t \t \n hello', function(event, code) {
-			console.log( event, new Buffer( code ) );
+			controller.emit( event, code );	
 		} );
-// 	} );
-// });
+	} );
+
+	test( 'trailingSpaces', function() {
+		var formatter = new Formatter()
+		  , controller = new Expector();
+
+		controller
+			.expect( 'ere', 'hello' )
+			.expect( 'format', '\t \t \n ' )
+			.expect( 'end' );
+			
+		formatter.forward( 'ere', 'hello\t \t', function(event, code) {
+			controller.emit( event, code );	
+		} );
+	});
+});
