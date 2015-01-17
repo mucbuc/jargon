433164b65666d84a10df23a53e597d4ef185d4c6
mbusenitz <mbusenitz@gmail.com>
2015-01-17 05:37:06 -0800

    dev formatter format

--- test/formatter.js
+++ test/formatter.js
@@ -7,31 +7,31 @@ var assert = require( 'assert' )
 assert( typeof Formatter === 'function' );
 
 suite( 'formatter', function() {
-	test( 'headingSpaces', function() {
-		var formatter = new Formatter()
-		  , controller = new Expector();
+  test( 'headingSpaces', function() {
+    var formatter = new Formatter()
+      , controller = new Expector();
 
-		controller
-			.expect( 'format', '\t \t \n ' )
-			.expect( 'ere', 'hello' )
-			.expect( 'end' );
+    controller
+      .expect( 'format', '\t \t \n ' )
+      .expect( 'ere', 'hello' )
+      .expect( 'end' );
 
-		formatter.forward( 'ere', '\t \t \n hello', function(event, code) {
-			controller.emit( event, code );	
-		} );
-	} );
+    formatter.forward( 'ere', '\t \t \n hello', function(event, code) {
+      controller.emit( event, code ); 
+    } );
+  } );
 
-	test( 'trailingSpaces', function() {
-		var formatter = new Formatter()
-		  , controller = new Expector();
+  test( 'trailingSpaces', function() {
+    var formatter = new Formatter()
+      , controller = new Expector();
 
-		controller
-			.expect( 'ere', 'hello' )
-			.expect( 'format', '\t \t \n ' )
-			.expect( 'end' );
-			
-		formatter.forward( 'ere', 'hello\t \t', function(event, code) {
-			controller.emit( event, code );	
-		} );
-	});
+    controller
+      .expect( 'ere', 'hello' )
+      .expect( 'format', '\t \t \n ' )
+      .expect( 'end' );
+      
+    formatter.forward( 'ere', 'hello\t \t', function(event, code) {
+      controller.emit( event, code ); 
+    } );
+  });
 });
