5a0800b294aa189ea779fe30ae19b5b61d83aae6
mbusenitz-blurb <mbusenitz@mac0232-mbusenitz.local>
2015-09-14 12:09:36 -0700

    tape commenter

--- test/commenter.js
+++ test/commenter.js
@@ -3,97 +3,105 @@
 var assert = require( 'chai' ).assert
   , Commenter = require( '../src/commenter' )
   , Expector = require( 'expector' ).SeqExpector
-  , fluke = require( 'flukejs' );
+  , fluke = require( 'flukejs' )
+  , tapeTest = require( 'tape' );
 
 assert( typeof Commenter === 'function' );
 
-suite( 'commenter', function() {
+var emitter;
 
-  var emitter;
-  setup(function() {
-    emitter = new Expector;
+function setup() {
+  emitter = new Expector;
+}
+
+function teardown(t) {
+  emitter.check(); 
+  delete emitter;
+  t.pass();
+  t.end();
+} 
+
+function test(name, foo) {
+  setup();
+  tapeTest(name, function(t) {
+    foo();
+    teardown(t); 
   });
-  
-  teardown(function() {
-    emitter.check(); 
-    delete emitter;
-  }); 
+}
 
-  test( 'commenterSingleLine', function(){
-    emitter
-      .expect( 'comment line' )
-      .expect( 'end' );
-    split( '// hello\n' );
-  });
-
-  test( 'commenterSingleLineWithoutNewLine', function(){
-    emitter
-      .expect( 'comment line' )
-      .expect( 'end' );
-    split( '// hello' );
-  });
-
-  test( 'commenterTwoSingleLineWithoutNewLine', function(){
-    emitter
-      .expect( 'comment line' )
-      .repeat( 1 )
-      .expect( 'end' );
-    split( '// hello\n//hello' );
-  });
-
-  test( 'commentBlockWithCommentLine', function() {
-    emitter
-      .expect( 'comment block', 'hello*/' )
-      .expect( 'comment line' )
-      .expect( 'end' );
-    split( '/*hello*/a//b' );
-  });
-
-  test( 'commentBlock', function() {
-    emitter
-      .expect( 'comment block' )
-      .expect( 'end' );
-    split( '/*hello*/' );
-  });
-
-  test( 'commentBlockWithNewLine', function() {
-    emitter
-      .expect( 'comment block', '\n*/' )
-      .expect( 'end' );
-    split( '/*\n*/' );
-  });
-
-  test( 'commentBlockWithConent', function() {
-    emitter
-      .expect( 'comment block', 'hello*/' )
-      .expect( 'end' );
-    split( '/*\nhello*/' );
-  });
-
-  function split( code ) {
-    var commenter = new Commenter()
-      , rules = {
-          'comment line': '\\/\\/',
-          'comment block': '\\/\\*',
-      }; 
-
-    fluke.splitAll( code, function( type, request ) {
-        if (type === 'comment line') {
-          commenter.processLine( request, function(val) {
-            emitter.emit( 'comment line', val );
-          } );
-        }
-        else if (type === 'comment block') {
-          commenter.processBlock( request, function(val) {
-            emitter.emit( 'comment block', val );
-          } );
-        }
-        else {
-          emitter.emit( type, request );
-        }
-      }
-      , rules ); 
-  }
-
+test( 'commenterSingleLine', function(){
+  emitter
+    .expect( 'comment line' )
+    .expect( 'end' );
+  split( '// hello\n' );
 });
+
+test( 'commenterSingleLineWithoutNewLine', function(){
+  emitter
+    .expect( 'comment line' )
+    .expect( 'end' );
+  split( '// hello' );
+});
+
+test( 'commenterTwoSingleLineWithoutNewLine', function(){
+  emitter
+    .expect( 'comment line' )
+    .repeat( 1 )
+    .expect( 'end' );
+  split( '// hello\n//hello' );
+});
+
+test( 'commentBlockWithCommentLine', function() {
+  emitter
+    .expect( 'comment block', 'hello*/' )
+    .expect( 'comment line' )
+    .expect( 'end' );
+  split( '/*hello*/a//b' );
+});
+
+test( 'commentBlock', function() {
+  emitter
+    .expect( 'comment block' )
+    .expect( 'end' );
+  split( '/*hello*/' );
+});
+
+test( 'commentBlockWithNewLine', function() {
+  emitter
+    .expect( 'comment block', '\n*/' )
+    .expect( 'end' );
+  split( '/*\n*/' );
+});
+
+test( 'commentBlockWithConent', function() {
+  emitter
+    .expect( 'comment block', 'hello*/' )
+    .expect( 'end' );
+  split( '/*\nhello*/' );
+});
+
+function split( code ) {
+  var commenter = new Commenter()
+    , rules = {
+        'comment line': '\\/\\/',
+        'comment block': '\\/\\*',
+    }; 
+
+  fluke.splitAll( code, function( type, request ) {
+      if (type === 'comment line') {
+        commenter.processLine( request, function(val) {
+          emitter.emit( 'comment line', val );
+        } );
+      }
+      else if (type === 'comment block') {
+        commenter.processBlock( request, function(val) {
+          emitter.emit( 'comment block', val );
+        } );
+      }
+      else {
+        emitter.emit( type, request );
+      }
+    }
+    , rules ); 
+}
 
