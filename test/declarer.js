f5211598d1459967ca16d58737dc6a667ff123cb
mbusenitz-blurb <mbusenitz@mac0232-mbusenitz.local>
2015-09-14 13:43:31 -0700

    declarer.js

--- test/declarer.js
+++ test/declarer.js
@@ -4,82 +4,70 @@ var assert = require( 'assert' )
   , Scoper = require( '../src/scoper' )
   , Declarer = require( '../src/declarer' )
   , Expector = require( 'expector' ).SeqExpector
-  , fluke = require( 'flukejs' ); 
+  , fluke = require( 'flukejs' )
+  , test = require( './base.js' ); 
 
 assert( typeof Declarer === 'function' );
 
-suite( 'declarer', function() {
-
-  var emitter;
-  setup(function() {
-    emitter = new Expector;
-  });
-
-  teardown(function() {
-    emitter.check(); 
-    delete emitter;
-  }); 
-
-  test( 'declareType', function(){
-    emitter
-      .expectNot( 'define type' )
-      .expect( 'statement' )
-      .expect( 'declare type' )
-      .expect( 'end' );
-    split( 'struct bla;' );
-  });
-
-  test( 'declareFunction', function(){
-    emitter
-      .expectNot( 'define function' )
-      .expect( 'statement' )
-      .expect( 'declare function', 'void foo()' )
-      .expect( 'end' );
-    split( 'void foo();' );
-  });
-
-  test( 'declareNot1', function(){
-    emitter
-      .expectNot( 'declare function' )
-      .expect( 'statement' )
-      .expect( 'code line' )
-      .expect( 'end' );
-    split( 'bla bla;' );
-  });
-  
-  test( 'declareNot2', function(){
-    emitter
-      .expectNot( 'declare function' )
-      .expect( 'statement' )
-      .expect( 'code line' )
-      .expect( 'end' );
-    split( 'bla += bla();' );
-  });
-
-  function split( code ) {
-    var rules = {
-        'statement': ';',
-        'open': '{'
-      }
-      , tokenizer = new Scoper( emitter, rules )
-      , declarer = new Declarer();
-      
-    fluke.splitAll( code, function( type, req ) {
-        emitter.emit( type, req );
-        if (type === 'statement') {
-          process( req );
-        }
-        else if (type === 'end') {
-          process( req );
-        }
-
-        function process( req ) { 
-          declarer.process( req, function( type, val ) {
-            emitter.emit( type, val );
-          });
-        }
-      }
-      , rules ); 
-    }
+test( 'declareType', function(emitter){
+  emitter
+    .expectNot( 'define type' )
+    .expect( 'statement' )
+    .expect( 'declare type' )
+    .expect( 'end' );
+  split( 'struct bla;', emitter );
 });
+
+test( 'declareFunction', function(emitter){
+  emitter
+    .expectNot( 'define function' )
+    .expect( 'statement' )
+    .expect( 'declare function', 'void foo()' )
+    .expect( 'end' );
+  split( 'void foo();', emitter );
+});
+
+test( 'declareNot1', function(emitter){
+  emitter
+    .expectNot( 'declare function' )
+    .expect( 'statement' )
+    .expect( 'code line' )
+    .expect( 'end' );
+  split( 'bla bla;', emitter );
+});
+
+test( 'declareNot2', function(emitter){
+  emitter
+    .expectNot( 'declare function' )
+    .expect( 'statement' )
+    .expect( 'code line' )
+    .expect( 'end' );
+  split( 'bla += bla();', emitter );
+});
+
+function split( code, emitter ) {
+  var rules = {
+      'statement': ';',
+      'open': '{'
+    }
+    , tokenizer = new Scoper( emitter, rules )
+    , declarer = new Declarer();
+    
+  fluke.splitAll( code, function( type, req ) {
+      emitter.emit( type, req );
+      if (type === 'statement') {
+        process( req );
+      }
+      else if (type === 'end') {
+        process( req );
+      }
+
+      function process( req ) { 
+        declarer.process( req, function( type, val ) {
+          emitter.emit( type, val );
+        });
+      }
+    }
+    , rules ); 
+}
 
