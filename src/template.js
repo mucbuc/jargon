71f676605405c5ba6561cdef2731b5517d37c132
mbusenitz <mbusenitz@gmail.com>
2017-12-10 09:53:04 -0800

    cleanup

--- src/template.js
+++ src/template.js
@@ -16,47 +16,13 @@ function Template() {
     
     assert( request.hasOwnProperty('resetStash') );
 
-    var rules = { 'open': '<', 'close': '>' };
-    var sub = new events.EventEmitter
+    const rules = { 'open': '<', 'close': '>' }
+      , sub = new events.EventEmitter
       , scoper = new Scoper( rules ); 
 
     scoper.process( request, (type, content) => {
       cb( request.lhs + rules.open + content.trim() + rules.close );
     }); 
-
-
-    //cb( request.lhs + request.token + request.rhs ); 
-
-/*
-    sub.on( 'open', function( req ) {
-      
-
-      scoper.process( req, function(type, content) {
-        sub.emit( type, content );
-      });
-    } );
-
-    //content += inner.lhs;
-
-    sub.on( 'close', function(code) {
-      cb( 'template parameters', code );
-      request.consume( code.length );
-      request.resetStash();
-    } );
-
-    sub.on( 'end', function( req ) {
-      cb( 'template parameters', req.lhs );
-      request.consume( req.lhs.length );
-      request.resetStash();
-    });
-*/
-    // fluke.splitAll( request.rhs, function( type, request) {
-    //       sub.emit( type, request );
-    //     }
-    //   , rules
-    // );
-
-
   };
 }
 
