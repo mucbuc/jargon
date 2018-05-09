02b8bd89cc9e5e1f4714f5a0f9b16fe6ab03f771
mbusenitz <mbusenitz@gmail.com>
2018-05-08 21:10:52 -0700

    base.js

---
+++ test/base.js
@@ -0,0 +1,9 @@
+let splitjs = require("./../src/split");
+
+function split(code, emitter) {
+  splitjs(code, (type, value) => {
+    emitter.emit(type, value);
+  });
+}
+
+module.exports = { split };
